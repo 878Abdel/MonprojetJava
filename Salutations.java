be78cc5738a454f96d7050c7e2600f2648fc9c98
Abdoulaye Ba <abdoulayeonokoko@gmail.com>
2025-11-26 00:13:46 +0000

    Mon premier code

---
+++ Salutations.java
@@ -0,0 +1,6 @@
+public class Salutations { 
+    public static void main(String[] args) {
+        //String nom = args[0];
+        System.out.println("Bienvenue et exellente année scolaire " );
+    }
+}
